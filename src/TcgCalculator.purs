a41faa9a64db1138b22bffca1053093ace07a6f1
acple <silentsphere110@gmail.com>
2025-03-20 01:13:46 +0900

    add normalization for condition patterns

--- src/TcgCalculator.purs
+++ src/TcgCalculator.purs
@@ -3,13 +3,13 @@ module TcgCalculator where
 import Prelude
 
 import Control.Alternative (empty)
-import Data.Array (all, any, concatMap, deleteBy, filter, find, foldMap, foldr, groupAllBy, length, nub, nubBy, replicate, sortBy, zipWith, (..))
+import Data.Array (all, any, concatMap, deleteBy, filter, find, findIndex, foldMap, groupAllBy, length, nub, nubBy, replicate, sortBy, updateAt, zipWith, (..), (:))
 import Data.Array.NonEmpty (foldl1, toArray)
 import Data.BigInt (BigInt)
-import Data.Foldable (and, fold, product)
+import Data.Foldable (and, fold, foldr, product)
 import Data.Function (on)
 import Data.Map as Map
-import Data.Maybe (maybe)
+import Data.Maybe (Maybe(..), maybe)
 import Data.Monoid.Additive (Additive(..))
 import Data.Newtype (alaF)
 import Data.Tuple (Tuple(..))
@@ -22,7 +22,7 @@ import TcgCalculator.Types (Card, Cards, Condition, ConditionGroup, ConditionMod
 calculate :: Deck -> ConditionSet -> BigInt
 calculate deck set = do
   let drawPattern = generateDrawPatterns deck
-  let conditionPatterns = buildConditionPattern =<< set
+  let conditionPatterns = normalizeConditionPatterns $ buildConditionPattern =<< set
   let pattern = filter (\dp -> any (satisfyCondition dp) conditionPatterns) drawPattern
   sumBy (calculatePatternCount deck) pattern
 
@@ -60,7 +60,8 @@ calculatePatternCount { others, hand } pattern = do
 
 ----------------------------------------------------------------
 
--- 各カード毎の引いて良い枚数条件を組み合わせた一つの条件式を表す
+-- カードとそのドロー可能枚数を組み合わせた一つの条件式を表す
+-- 暗黙的に card.id で昇順にソートされていることを期待する
 type ConditionPattern = Array { card :: Card, min :: Int, max :: Int }
 
 satisfyCondition :: DrawPattern -> ConditionPattern -> Boolean
@@ -86,50 +87,62 @@ mergeConditionPattern left right = foldl1 merge <$> groupAllBy (comparing _.card
 isValidConditionPattern :: ConditionPattern -> Boolean
 isValidConditionPattern = all \{ card: { count }, min, max } -> min <= max && min <= count
 
+-- 重複する条件を取り除いてパターンを正規化する
+normalizeConditionPatterns :: Array ConditionPattern -> Array ConditionPattern
+normalizeConditionPatterns = groupAllBy (comparing $ map _.card.id) >=> foldr collect []
+  where
+  collect condition patterns = if any (include condition) patterns -- より広い条件を既に含む場合は何もしない
+    then patterns
+    else case findIndex (flip include condition) patterns of -- より広い条件が与えられた場合は上書きする
+      Just i -> fold $ updateAt i condition patterns
+      Nothing -> condition : patterns
+  include = map and <<< zipWith \{ min: min1, max: max1 } { min: min2, max: max2 } -> min2 <= min1 && max1 <= max2
+
 -- 一つの Condition に対応する全パターンのリストを出力する
 mkConditionPattern :: Condition -> Array ConditionPattern
-mkConditionPattern { mode, count, cards } = case mode of
-  -- cards の中から count 枚以上を引くパターン
-  AtLeast -> ado
-    pattern <- mkDrawPattern cards count
-    in pattern <#> \p -> { card: p.card, min: p.draw, max: p.card.count }
-  -- cards の中からちょうど count 枚を引くパターン
-  JustDraw -> ado
-    pattern <- mkDrawPattern cards count
-    in cards <#> \card -> do
-      let draw = maybe 0 _.draw $ find (_.card.id >>> (_ == card.id)) pattern
-      { card, min: draw, max: draw }
-  -- count 枚以上デッキに残すパターン
-  Remains -> ado
-    pattern <- mkDrawPattern cards (sumBy _.count cards - count)
-    in cards <#> \card -> do
-      let draw = maybe 0 _.draw $ find (_.card.id >>> (_ == card.id)) pattern
-      { card, min: 0, max: draw }
-  -- ちょうど count 枚デッキに残すパターン
-  JustRemains ->
-    mkConditionPattern { mode: JustDraw, count: (sumBy _.count cards - count), cards }
-  -- cards の中から count 種類以上を1枚以上引くパターン
-  Choice -> ado
-    pattern <- mkDrawPattern' cards [replicate count 1]
-    in pattern <#> \p -> { card: p.card, min: 1, max: p.card.count }
-  -- cards の中から count 種類以上を1枚以上残すパターン
-  LeftOne -> ado
-    pattern <- mkDrawPattern' cards [replicate count 1]
-    in pattern <#> \p -> { card: p.card, min: 0, max: p.card.count - 1 }
-  -- cards の中から count 種類以上を1枚も引かないパターン
-  LeftAll -> ado
-    pattern <- mkDrawPattern' cards [replicate count 0]
-    in pattern <#> \p -> { card: p.card, min: 0, max: 0 }
+mkConditionPattern { mode, count, cards } = do
+  let cards' = sortBy (comparing _.id) cards
+  case mode of
+    -- cards の中から count 枚以上を引くパターン
+    AtLeast -> ado
+      pattern <- mkDrawPattern cards' count
+      in pattern <#> \p -> { card: p.card, min: p.draw, max: p.card.count }
+    -- cards の中からちょうど count 枚を引くパターン
+    JustDraw -> ado
+      pattern <- mkDrawPattern cards' count
+      in cards' <#> \card -> do
+        let draw = maybe 0 _.draw $ find (_.card.id >>> (_ == card.id)) pattern
+        { card, min: draw, max: draw }
+    -- count 枚以上デッキに残すパターン
+    Remains -> ado
+      pattern <- mkDrawPattern cards' (sumBy _.count cards' - count)
+      in cards' <#> \card -> do
+        let draw = maybe 0 _.draw $ find (_.card.id >>> (_ == card.id)) pattern
+        { card, min: 0, max: draw }
+    -- ちょうど count 枚デッキに残すパターン
+    JustRemains ->
+      mkConditionPattern { mode: JustDraw, count: (sumBy _.count cards' - count), cards: cards' }
+    -- cards の中から count 種類以上を1枚以上引くパターン
+    Choice -> ado
+      pattern <- mkDrawPattern' cards' [replicate count 1]
+      in pattern <#> \p -> { card: p.card, min: 1, max: p.card.count }
+    -- cards の中から count 種類以上を1枚以上残すパターン
+    LeftOne -> ado
+      pattern <- mkDrawPattern' cards' [replicate count 1]
+      in pattern <#> \p -> { card: p.card, min: 0, max: p.card.count - 1 }
+    -- cards の中から count 種類以上を1枚も引かないパターン
+    LeftAll -> ado
+      pattern <- mkDrawPattern' cards' [replicate count 0]
+      in pattern <#> \p -> { card: p.card, min: 0, max: 0 }
 
 -- カードを指定枚数引く全ての組み合わせを列挙する
 mkDrawPattern :: Cards -> Int -> Array DrawPattern
 mkDrawPattern cards count = mkDrawPattern' cards $ partitionNumber count
 
 -- 指定の枚数パターンに合致するカードの組み合わせを全て列挙する
+-- 引数 cards の順序は維持される
 -- 引数 pattern の各要素は予め降順にソートされている必要がある
 mkDrawPattern' :: Cards -> Array (Array Int) -> Array DrawPattern
-mkDrawPattern' _ [] = []
-mkDrawPattern' _ [[]] = [[]]
 mkDrawPattern' cards pattern = do
   let cardsLength = length cards
   let cardCounts = sortBy (flip compare) $ _.count <$> cards
