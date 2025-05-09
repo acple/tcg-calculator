87437e531898f2fe559dd8eef2c8497106772f9f
acple <silentsphere110@gmail.com>
2025-05-10 00:34:52 +0900

    rewrite appState codec

--- src/TcgCalculator/Codec.purs
+++ src/TcgCalculator/Codec.purs
@@ -8,7 +8,8 @@ import Prelude
 
 import Codec.JSON.DecodeError (DecodeError)
 import Codec.JSON.DecodeError as DecodeError
-import Control.Monad.Except (ExceptT(..), except, runExcept)
+import Control.Monad.Except (ExceptT, except, lift, runExcept)
+import Control.Monad.Reader (ReaderT, ask, runReaderT)
 import Data.Array (elemIndex, sort, (!!))
 import Data.Codec (Codec', codec', hoist, (<~<))
 import Data.Codec (encode, decode) as Export
@@ -17,16 +18,10 @@ import Data.Codec.JSON.Common as CJC
 import Data.Codec.JSON.Record as CJR
 import Data.Codec.JSON.Sum as CJS
 import Data.Either (note)
-import Data.Functor.Compose (Compose(..))
-import Data.Newtype (un)
-import Data.Traversable (fold, for, sequence, traverse)
+import Data.Traversable (fold, traverse)
 import Effect (Effect)
-import Effect.Class (class MonadEffect)
 import JSON (JSON)
-import Prim.Row as Row
-import Record as Record
-import TcgCalculator.Types (AppState, AppStateJson, CardJson, ConditionGroupJson, ConditionMode, ConditionSetJson, DeckJson, Id, generateId, readConditionMode)
-import Type.Proxy (Proxy(..))
+import TcgCalculator.Types (AppCondition, AppConditionGroup, AppConditionSet, AppState, AppStateJson, Card, CardId, CardJson, ConditionGroupJson, ConditionJson, ConditionMode, ConditionSetJson, DeckJson, generateId, readConditionMode)
 
 ----------------------------------------------------------------
 
@@ -39,28 +34,46 @@ appState' = codec' decode encode
 
   decode :: AppStateJson -> ExceptT DecodeError Effect AppState
   decode { deck: { cards, hand, others }, condition: set } = do
-    cards' <- traverse insertId cards
-    let ids = cards' <#> _.id
-    conditions <- ExceptT <<< sequence <<< note (DecodeError.basic "Could not decode conditions") <<< un Compose $ do
-      for set \group -> do
-        { conditions: _, disabled: group.disabled } <$> for group.conditions \{ mode, count, cards: indexes, disabled } -> do
-          Compose $ insertId <<< { condition: _, disabled } <<< { mode, count, cards: _ } <<< sort <$> traverse (ids !! _) indexes
-    set' <- traverse insertId conditions
-    pure { deck: { cards: cards', hand, others }, condition: set' }
+    cards' <- traverse decodeCard cards
+    condition <- runReaderT (decodeConditionSet set) (cards' <#> _.id)
+    pure { deck: { cards: cards', hand, others }, condition }
 
-  insertId :: forall m r a. Row.Lacks "id" r => MonadEffect m => { | r } -> m { id :: Id a | r }
-  insertId = flap $ Record.insert (Proxy @"id") <$> generateId
+  decodeCard :: CardJson -> _ Card
+  decodeCard { name, count } = { id: _, name, count } <$> generateId
+
+  decodeConditionSet :: ConditionSetJson -> _ AppConditionSet
+  decodeConditionSet = traverse \{ conditions, disabled } ->
+    { id: _, conditions: _, disabled } <$> generateId <*> decodeConditionGroup conditions
+
+  decodeConditionGroup :: ConditionGroupJson -> _ AppConditionGroup
+  decodeConditionGroup = traverse decodeCondition
+
+  decodeCondition :: ConditionJson -> ReaderT (Array CardId) _ AppCondition
+  decodeCondition { mode, count, cards, disabled } = do
+    ids <- ask
+    id <- generateId
+    cards' <- lift <<< except <<< note (DecodeError.basic "Could not decode conditions") <<< traverse (ids !! _) $ cards
+    pure { id, condition: { mode, count, cards: cards' }, disabled }
 
   encode :: AppState -> AppStateJson
   encode { deck: { cards, hand, others }, condition: set } = do
-    let ids = cards <#> _.id
-    let set' = set <#> \{ conditions, disabled: groupDisabled } -> do
-          { conditions: conditions <#> \{ condition: { mode, count, cards: selected }, disabled } ->
-              { mode, count, cards: sort <<< fold $ traverse (elemIndex <@> ids) selected, disabled }
-          , disabled: groupDisabled
-          }
-    let cards' = cards <#> \{ name, count } -> { name, count }
-    { deck: { cards: cards', hand, others }, condition: set' }
+    let cards' = encodeCard <$> cards
+    let condition = encodeConditionSet set (cards <#> _.id)
+    { deck: { cards: cards', hand, others }, condition }
+
+  encodeCard :: Card -> CardJson
+  encodeCard { name, count } = { name, count }
+
+  encodeConditionSet :: AppConditionSet -> _ ConditionSetJson
+  encodeConditionSet = traverse \{ conditions, disabled } -> { conditions: _, disabled } <$> encodeConditionGroup conditions
+
+  encodeConditionGroup :: AppConditionGroup -> _ ConditionGroupJson
+  encodeConditionGroup = traverse encodeCondition
+
+  encodeCondition :: AppCondition -> Array CardId -> ConditionJson
+  encodeCondition { condition: { mode, count, cards }, disabled } ids = do
+    let cards' = sort <<< fold $ traverse (elemIndex <@> ids) cards
+    { mode, count, cards: cards', disabled }
 
 ----------------------------------------------------------------
 
